fa701fa99c9b99af5795689f673c3041b1841132
yolno0m0 <yuuworks.0825@gmail.com>
2023-04-04 23:03:15 +0900

    カリキュラム1.13.1

---
+++ HelloWorld/src/main/curriculum1_13_1.java
@@ -0,0 +1,25 @@
+/*
+	int型の配列aを作成してください。
+	配列aに1～5の数字を格納してください。
+	繰り返し処理を使用し、
+	配列aの数字を順番に表示させてください。
+
+	出力結果：1
+			  2
+			  3
+			  4
+			  5
+*/
+package main;
+
+public class curriculum1_13_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int [] a = {1,2,3,4,5};
+		for(int i=0 ; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
