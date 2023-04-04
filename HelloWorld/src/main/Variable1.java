24500da934a4cadbb82a60ddb51bc8deb2f1686d
yolno0m0 <yuuworks.0825@gmail.com>
2023-04-04 10:46:29 +0900

    Variable1修正

--- HelloWorld/src/main/Variable1.java
+++ HelloWorld/src/main/Variable1.java
@@ -18,16 +18,17 @@ public class Variable1 {
 		name ="福田優海";
 		String gender;
 		gender = "女";
-		int age;
-		age = 25;
+		String age;
+		age = "25";
 		double height;
 		height = 152.3;
+		String str = String .valueOf(height);
 		
 		System.out.println("私の自己紹介をします");
 		System.out.println("名前は"+name+"です。");
 		System.out.println("性別は"+gender+"です。");
 		System.out.println("年齢は"+age+"です。");
-		System.out.println("身長は"+height+"です。");
+		System.out.println("身長は"+str+"です。");
 		System.out.println("よろしくお願いします。");
 	}
 }
