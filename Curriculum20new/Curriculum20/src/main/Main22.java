9681e9c74a0b3a93a991d2e4e0191ef57dc1fcd4
yolno0m0 <yuuworks.0825@gmail.com>
2023-04-13 10:24:56 +0900

    カリキュラム22＜修正後＞

---
+++ Curriculum20new/Curriculum20/src/main/Main22.java
@@ -0,0 +1,14 @@
+package main;
+import main.frameork.Frame22;
+public class Main22 {
+	public static void main(String args[]){
+		Frame22 f = new Frame22();
+		
+		
+		//テキストメソッドの呼び出し
+		f.text();
+		//現在時刻を表示
+		String nowtime = f.time();
+		System.out.println("現在の時刻は"+nowtime+"です");
+	}
+	}	
