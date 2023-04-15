103740370b6f969428701eeac77d410224b07210
yolno0m0 <yuuworks.0825@gmail.com>
2023-04-15 20:21:05 +0900

    カリキュラム1.22<コメント追加・インシデント整理>

--- Curriculum20new/Curriculum20/src/main/Main22.java
+++ Curriculum20new/Curriculum20/src/main/Main22.java
@@ -2,9 +2,8 @@ package main;
 import main.frameork.Frame22;
 public class Main22 {
 	public static void main(String args[]){
+		//インスタンス化
 		Frame22 f = new Frame22();
-		
-		
 		//テキストメソッドの呼び出し
 		f.text();
 		//現在時刻を表示
--- Curriculum20new/Curriculum20/src/main/frameork/Frame22.java
+++ Curriculum20new/Curriculum20/src/main/frameork/Frame22.java
@@ -2,28 +2,31 @@ package main.frameork;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 public class Frame22{
-		String greet;
-		String sushi;
-		String food;
+	//「こんにちは！ここは日本です！」代入変数
+	String greet;
+	//「この寿司はうまい」代入変数
+	String sushi;
+	//「寿司は和食です」代入変数
+	String food;
 	
-	public String time() {
+public String time() {
 	//現在日時を取得	
 	LocalDateTime date1 =	LocalDateTime.now();
 	//表示形式を設定
 	DateTimeFormatter dtformat =
-			DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 	String fdate1 = dtformat.format(date1);
 	return fdate1;
 	}
 	
-	public void text(){
-		//変数へ代入
-		this.greet = "こんにちは！ここは日本です！";
-		this.sushi = "この寿司はうまい";
-		this.food = "寿司は和食です";
-		//表示
-		System.out.println(greet);
-		System.out.println(sushi);
-		System.out.println(food);
+public void text(){
+	//変数へ代入
+	this.greet = "こんにちは！ここは日本です！";
+	this.sushi = "この寿司はうまい";
+	this.food = "寿司は和食です";
+	//表示
+	System.out.println(greet);
+	System.out.println(sushi);
+	System.out.println(food);
 	}
 }
