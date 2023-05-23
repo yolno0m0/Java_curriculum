f0e82ec095298fdef27f3cb8e93180a84585ba3d
yolno0m0 <yuuworks.0825@gmail.com>
2023-05-23 14:12:30 +0900

    1.29<修正>0523

--- HelloWorld/src/curriculum28_29/array/Array.java
+++ HelloWorld/src/curriculum28_29/array/Array.java
@@ -21,8 +21,8 @@ public class Array {
         countries.put(8, "栃木県:宇都宮市:6408");
         countries.put(9, "群馬県:前橋市:6362");
         countries.put(10, "埼玉県:さいたま市:3798");
-        
-        //scannerで入力した配列をInteger型に変換
+
+        // scannerで入力した配列をInteger型に変換
         Integer[] sortedArrays = Stream.of(inputIndex).mapToInt(Integer::parseInt)
                 .boxed().toArray(Integer[]::new);
 
@@ -32,15 +32,15 @@ public class Array {
         } else if (sortArrays.equals("降順")) {
             Arrays.sort(sortedArrays, Collections.reverseOrder());
         }
-        
-        //sorteArraysで受け取った配列を繰り返し処理でキーとして返す
-        for (Integer index : sortedArrays ) {
-        	String[] date = countries.get(index).split(":");
-        	
-        	//面積をdouble型に変換する
+
+        // sortedArraysで受け取った配列を繰り返し処理でキーとして返す
+        for (Integer index : sortedArrays) {
+            String[] date = countries.get(index).split(":");
+
+            // 面積をdouble型に変換する
             String area = date[2];
             double dbArea = Double.parseDouble(area);
-           
+
             // Map Keyとscannerで格納された配列の値を比較して表示させる
             System.out.println("");
             System.out.println("都道府県：" + date[0]);
